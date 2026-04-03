b4bcd373dcf9cb4155992b7958b74fd76ca9e417
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2026-04-03 09:28:49 +0200

    feat: add missing outputs (#33)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,18 @@ output "databases" {
   description = "Contains all sql managed instance databases"
   value       = azurerm_mssql_managed_database.databases
 }
+
+output "security_alert_policy" {
+  description = "Contains all sql managed instance security alert policy configuration"
+  value       = azurerm_mssql_managed_instance_security_alert_policy.policy
+}
+
+output "vulnerability_assessment" {
+  description = "Contains all sql managed instance vulnerability assessment configuration"
+  value       = azurerm_mssql_managed_instance_vulnerability_assessment.assessment
+}
+
+output "ad_admin" {
+  description = "Contains all sql managed instance active directory administrator configuration"
+  value       = azurerm_mssql_managed_instance_active_directory_administrator.sql
+}
