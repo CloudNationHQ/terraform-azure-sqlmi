f225af4e590d7722168a574a6578315a3a545747
dkooll <dennis.kool@cloudnation.nl>
2026-09-08 08:17:41 +0200

    feat: azurerm provider 5 upgrade BREAKING CHANGE: this change causes recreates

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "config" {
+variable "mssql_managed_instance" {
   description = "Contains all sql managed instance configuration"
   type = object({
     name                           = string
@@ -10,34 +10,42 @@ variable "config" {
     resource_group_name            = optional(string)
     location                       = optional(string)
     license_type                   = optional(string, "LicenseIncluded")
-    administrator_login            = optional(string, "adminLogin")
-    collation                      = optional(string, "SQL_Latin1_General_CP1_CI_AS")
-    database_format                = optional(string, "SQLServer2022")
+    administrator_login            = optional(string)
+    collation                      = optional(string)
+    database_format                = optional(string)
     dns_zone_partner_id            = optional(string)
-    hybrid_secondary_usage         = optional(string, "Active")
-    maintenance_configuration_name = optional(string, "SQL_Default")
-    minimum_tls_version            = optional(string, "1.2")
-    proxy_override                 = optional(string, "Default")
-    public_data_endpoint_enabled   = optional(bool, false)
+    hybrid_secondary_usage         = optional(string)
+    maintenance_configuration_name = optional(string)
+    minimum_tls_version            = optional(string)
+    proxy_override                 = optional(string)
+    public_data_endpoint_enabled   = optional(bool)
     service_principal_type         = optional(string)
-    storage_account_type           = optional(string, "GRS")
-    zone_redundant_enabled         = optional(bool, false)
-    timezone_id                    = optional(string, "UTC")
+    storage_account_type           = optional(string)
+    zone_redundant_enabled         = optional(bool)
+    timezone_id                    = optional(string)
+    general_purpose_v2_enabled     = optional(bool)
+    storage_iops                   = optional(number)
     tags                           = optional(map(string))
     identity = optional(object({
       type         = string
       identity_ids = optional(list(string), [])
+    }))
+    azure_active_directory_administrator = optional(object({
+      login_username                      = string
+      object_id                           = string
+      principal_type                      = string
+      azuread_authentication_only_enabled = optional(bool, false)
+      tenant_id                           = optional(string)
     }))
     databases = optional(map(object({
       name                      = string
       short_term_retention_days = optional(number, 7)
       tags                      = optional(map(string))
       long_term_retention_policy = optional(object({
-        weekly_retention          = optional(string)
-        monthly_retention         = optional(string)
-        yearly_retention          = optional(string)
-        week_of_year              = optional(number)
-        immutable_backups_enabled = optional(bool, false)
+        weekly_retention  = optional(string)
+        monthly_retention = optional(string)
+        yearly_retention  = optional(string)
+        week_of_year      = optional(number)
       }))
       point_in_time_restore = optional(object({
         source_database_id    = string
@@ -45,21 +53,21 @@ variable "config" {
       }))
     })), {})
     security_alert_policy = optional(object({
-      enabled                      = optional(bool, true)
-      storage_endpoint             = string
-      storage_account_access_key   = string
-      retention_days               = optional(number, 30)
-      email_account_admins_enabled = optional(bool, true)
+      enabled                      = optional(bool)
+      storage_endpoint             = optional(string)
+      storage_account_access_key   = optional(string)
+      retention_days               = optional(number)
+      email_account_admins_enabled = optional(bool)
       email_addresses              = optional(list(string), [])
       disabled_alerts              = optional(list(string), [])
     }))
     vulnerability_assessment = optional(object({
       storage_container_path     = string
-      storage_account_access_key = string
+      storage_account_access_key = optional(string)
       storage_container_sas_key  = optional(string)
       recurring_scans = optional(object({
-        enabled                   = optional(bool, true)
-        email_subscription_admins = optional(bool, true)
+        enabled                   = optional(bool)
+        email_subscription_admins = optional(bool)
         emails                    = optional(list(string), [])
         }), {
         enabled                   = true
@@ -68,12 +76,32 @@ variable "config" {
       })
     }))
     ad_admin = optional(object({
-      principal_type              = string # "User", "Group", or "ServicePrincipal"
+      principal_type              = string
       tenant_id                   = optional(string)
       azuread_authentication_only = optional(bool, false)
       object_id                   = optional(string)
       display_name                = optional(string)
-      user_principal_name         = optional(string) # For User type
+      client_id                   = optional(string)
+      user_principal_name         = optional(string)
+      mail                        = optional(string)
+      employee_id                 = optional(string)
+      mail_nickname               = optional(string)
+      mail_enabled                = optional(bool)
+      security_enabled            = optional(bool)
+      include_transitive_members  = optional(bool)
+      directory_role = optional(object({
+        display_name = optional(string, "Directory Readers")
+        template_id  = optional(string)
+      }), {})
+      directory_role_assignment = optional(object({
+        app_scope_id       = optional(string)
+        directory_scope_id = optional(string)
+      }), {})
+      time_sleep = optional(object({
+        create_duration  = optional(string, "10s")
+        destroy_duration = optional(string)
+        triggers         = optional(map(string))
+      }), {})
     }))
   })
 }
@@ -88,6 +116,11 @@ variable "resource_group_name" {
   description = "default resource group to be used."
   type        = string
   default     = null
+
+  validation {
+    condition     = var.resource_group_name == null || can(regex("^[a-zA-Z0-9_.()-]{1,90}$", var.resource_group_name))
+    error_message = "Resource group name must be 1-90 characters and can only include alphanumeric characters, underscores, parentheses, hyphens, and periods."
+  }
 }
 
 variable "tags" {
